56399d87355948eea984ad7271487b944fd485e1
Anatoliy Chakkaev <mail@anatoliy.in>
2016-08-27 00:13:27 +0300

    Save state with debounce

--- store.js
+++ store.js
@@ -3,5 +3,27 @@
 import { createStore } from 'redux';
 import reducers from './reducers';
 
-export default createStore(reducers);
+let savedState;
+
+try {
+    savedState = JSON.parse(localStorage.state);
+} catch (e) {
+    console.log('No saved state');
+}
+
+const store = createStore(reducers, savedState);
+
+store.subscribe(() => {
+    const state = store.getState();
+    const { errors } = state;
+    localStorage.state = JSON.stringify({
+        ...state,
+        errors: {
+            ...errors,
+            items: []
+        }
+    });
+});
+
+export default store;
 
